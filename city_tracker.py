fa840fa49f2fa15627d39d5e2b183fe69936dac6
Phil Hagen <phil@lewestech.com>
2021-02-11 20:31:43 -0500

    first commit - fully functional

---
+++ city_tracker.py
@@ -0,0 +1,99 @@
+#!/usr/bin/env python3
+# (C)2021 Phil Hagen <phil@lewestech.com>
+#
+# This script will read a list of records in JSON format that describe one location each.
+# It will then determine the sunset and sunrise times for each location and creat an "at" job at each of those events.
+#
+# All clocks are assumed UTC because if they are not, they are wrong.
+
+import time
+import requests
+import tempfile
+import subprocess
+import json
+import datetime
+# these 3 lines are needed for python <3.7
+from backports.datetime_fromisoformat import MonkeyPatch
+MonkeyPatch.patch_fromisoformat()
+import argparse
+
+default_log = '/var/log/city_tracker.log'
+
+parser = argparse.ArgumentParser()
+parser.add_argument('-v', '--verbose', help='Verbose mode', action='store_true', default=False)
+parser.add_argument('-t', '--test', help='Run in test mode - do not create jobs', action='store_true', default=False)
+parser.add_argument('-L', '--nolog', dest='log', help='Disable logging', action='store_false', default=True)
+parser.add_argument('-f', '--logfile', help='Specify the logfile location if logging is enabled', default=default_log)
+args = parser.parse_args()
+
+if args.test:
+    args.log = False
+
+cf = open('/etc/sysconfig/city_config.json', 'r')
+cities = json.load(cf)
+cf.close()
+
+request_headers = { 'user-agent': 'Lego ISS tracker', 'accept': 'application/json' }
+
+current_time = datetime.datetime.now(datetime.timezone.utc)
+
+if args.log:
+    logfile = open(args.logfile, 'a')
+    
+logtime = datetime.datetime.utcnow().isoformat()
+
+for city in cities:
+    city_name = city['name']
+    latitude = city['latitude']
+    longitude = city['longitude']
+    lights = city['lights']
+
+    city_sun_url = 'https://api.sunrise-sunset.org/json?lat=%f&lng=%f&formatted=0' % (latitude, longitude)
+
+    city_sun_data = requests.get(city_sun_url, headers=request_headers)
+    city_object = city_sun_data.json()['results']
+
+    sunrise = datetime.datetime.fromisoformat(city_object['sunrise'])
+    sunset = datetime.datetime.fromisoformat(city_object['sunset'])
+
+    time_to_sunrise = sunrise - current_time
+    time_to_sunset = sunset - current_time
+
+    if time_to_sunrise.total_seconds() < 0 or time_to_sunset.total_seconds() < 0:
+        city_sun_url = 'https://api.sunrise-sunset.org/json?lat=%f&lng=%f&formatted=0&date=tomorrow' % (latitude, longitude)
+
+        city_sun_data_tomorrow = requests.get(city_sun_url, headers=request_headers)
+        city_object_tomorrow = city_sun_data_tomorrow.json()['results']
+
+        if time_to_sunrise.total_seconds() < 0:
+            sunrise = datetime.datetime.fromisoformat(city_object_tomorrow['sunrise'])
+            time_to_sunrise = sunrise - current_time
+
+        if time_to_sunset.total_seconds() < 0:
+            sunset = datetime.datetime.fromisoformat(city_object_tomorrow['sunset'])
+            time_to_sunset = sunset - current_time
+
+    for light in lights:
+        if args.verbose:
+            print('%s Turning %s:%s on at %s (%d min) and off at %s (%d min)' % (logtime, city_name, light, sunset.isoformat(), int(time_to_sunset.total_seconds()/60), sunrise.isoformat(), int(time_to_sunrise.total_seconds()/60)))
+
+        if args.log:
+            logfile.write('%s %s:%s Turning on at %s and off at %s\n' % (logtime, city_name, light, sunset.isoformat(), sunrise.isoformat()))
+
+
+        if not args.test:
+            fh1 = tempfile.TemporaryFile()
+            fh1.write(bytes('/usr/local/bin/homeScript.py -s %s 0' % (light), 'utf-8'))
+            fh1.seek(0)
+            fh2 = tempfile.TemporaryFile()
+            fh2.write(bytes('/usr/local/bin/homeScript.py -s %s 1' % (light), 'utf-8'))
+            fh2.seek(0)
+
+            subprocess.run(['at', '-M', 'now', '+', str(int(time_to_sunrise.total_seconds()/60)), 'minutes'], stdin=fh1, stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
+            subprocess.run(['at', '-M', 'now', '+', str(int(time_to_sunset.total_seconds()/60)), 'minutes'], stdin=fh2, stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
+
+            fh1.close()
+            fh2.close()
+
+if args.log:
+    logfile.close()
---
+++ iss_tracker.py
@@ -0,0 +1,106 @@
+#!/usr/bin/env python3
+# (C)2021 Phil Hagen <phil@lewestech.com>
+#
+# This script will read determine the next seven minutes of the ISS's location, to include whether the ISS is in daylight or shadow.
+# It will then create an "at" job for the minute when the ISS enters or exits the shadow.
+#
+# All clocks are assumed UTC because if they are not, they are wrong.
+
+import time
+import requests
+import tempfile
+import subprocess
+import datetime
+import argparse
+
+default_log = '/var/log/iss_tracker.log'
+run_every = 6
+
+parser = argparse.ArgumentParser()
+parser.add_argument('-v', '--verbose', help='Verbose mode', action='store_true', default=False)
+parser.add_argument('-t', '--test', help='Run in test mode - do not create jobs', action='store_true', default=False)
+parser.add_argument('-L', '--nolog', dest='log', help='Disable logging', action='store_false', default=True)
+parser.add_argument('-f', '--logfile', help='Specify the logfile location if logging is enabled', default=default_log)
+args = parser.parse_args()
+
+if args.test:
+    args.log = False
+
+# build a list of timestamps: current time and (run_every+1) minutes after that
+ts = []
+ts.append(int(time.time()))
+
+for i in range(1,run_every+1):
+    ts.append(int(ts[0] + i*60))
+
+# convert the timestamps to strings
+ts = [str(int) for int in ts]
+
+# get current and next 9 locations for the ISS
+iss_positions_url = 'https://api.wheretheiss.at/v1/satellites/25544/positions?units=miles&timestamps=%s&units=miles' % (','.join(ts))
+
+request_headers = { 'user-agent': 'Lego ISS tracker', 'accept': 'application/json' }
+iss_loc = requests.get(iss_positions_url, headers=request_headers)
+
+location_object = iss_loc.json()
+
+if args.log:
+    logfile = open(args.logfile, 'a')
+    logtime = datetime.datetime.utcnow().isoformat()
+
+light_state_changed = False
+
+for i in range(0,run_every):
+    timestamp1 = int(ts[i])
+    timestamp2 = int(ts[i+1])
+
+    iss_sun_state1 = location_object[i]['visibility']
+    iss_sun_state2 = location_object[i+1]['visibility']
+
+    if args.verbose:
+        print('%d - %d: %s' % (timestamp1, timestamp2, iss_sun_state2))
+
+    if iss_sun_state1 != iss_sun_state2:
+        light_state_changed = True
+
+        if iss_sun_state2 == 'eclipsed':
+            if args.verbose:
+                print('ISS eclipses in %d minutes' % (i))
+
+            if args.log:
+                logfile.write('%s: ISS eclipses in %d minutes\n' % (logtime, i))
+
+            if not args.test:
+                fh = tempfile.TemporaryFile()
+                fh.write(b'/usr/local/bin/homeScript.py -s Lego_ISS_Lights 1')
+                fh.seek(0)
+
+                subprocess.run(['at', '-M', 'now', '+', str(i+1), 'minutes'], stdin=fh, stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
+
+                fh.close()
+
+        elif iss_sun_state2 == 'daylight':
+            if args.verbose:
+                print('ISS in daylight in %d minutes' % (i))
+            
+            if args.log:
+                logfile.write('%s ISS in daylight in %d minutes\n' % (logtime, i))
+
+            if not args.test:
+                fh = tempfile.TemporaryFile()
+                fh.write(b'/usr/local/bin/homeScript.py -s Lego_ISS_Lights 0')
+                fh.seek(0)
+
+                subprocess.run(['at', '-M', 'now', '+', str(i+1), 'minutes'], stdin=fh, stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
+
+                fh.close()
+
+if not light_state_changed:
+    if args.verbose:
+        print('Daylight state NOT changed in next %d minutes - no jobs created' % (run_every))
+    
+    if args.log:
+        logfile.write('%s Daylight state NOT changed in next %d minutes - no jobs created\n' % (logtime, run_every))
+
+if args.log:
+    logfile.close()
